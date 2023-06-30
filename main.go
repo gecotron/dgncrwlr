7bc59f8ed8724e316a38358c7b485e1b22f8beac
Michael Ward <gecotron@gmail.com>
2023-06-30 12:09:20 -0400

    functional flags

--- main.go
+++ main.go
@@ -7,10 +7,11 @@ import (
 
 func main() {
 	var rows int
-	var cols int
-	flag.IntVar(&rows, "rows", 20, "Number of rows wanted")
-	flag.IntVar(&cols, "Columns", 20, "Number of columns wanted")
-	gameDungeon := initDungeon(rows, cols)
+	var columns int
+	flag.IntVar(&rows, "r", 20, "Number of rows wanted")
+	flag.IntVar(&columns, "c", 20, "Number of columns wanted")
+	flag.Parse()
+	gameDungeon := initDungeon(rows, columns)
 	for i := 0; i < len(gameDungeon.rooms); i++ {
 		for x := 0; x < len(gameDungeon.rooms[i]); x++ {
 			fmt.Printf("%c ", gameDungeon.rooms[i][x])
